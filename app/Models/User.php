4ed2ce8a007afae421aac247fee2f87d806bfe01
dochinhthtn <dochinhthtn@gmail.com>
2021-03-23 23:13:47 +0700

    Add relations: subjects, posts, profile, role, rates

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use PDO;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     use HasFactory, Notifiable;
 
     /**
@@ -40,4 +39,28 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function subjects() {
+        return $this->belongsToMany(Subject::class, 'users_subjects', 'user_id', 'subject_id');
+    }
+
+    public function posts() {
+        return $this->hasMany(Post::class, 'user_id');
+    }
+
+    public function appliedPosts() {
+        return $this->belongsToMany(Post::class, 'applications', 'user_id', 'post_id');
+    }
+
+    public function profile() {
+        return $this->hasOne(Profile::class, 'user_id');
+    }
+
+    public function role() {
+        return $this->belongsTo(Role::class, 'role_id');
+    }
+
+    public function rates() {
+        return $this->hasMany(Rate::class, 'tutor_id');
+    }
 }
