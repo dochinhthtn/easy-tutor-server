f0b2eb4e571f2f9ad37cda45993d378b17e5324a
dochinhthtn <dochinhthtn@gmail.com>
2021-03-23 23:14:32 +0700

    Add tables

---
+++ database/migrations/2021_03_23_063740_create_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('sex', 191);
+            $table->string('address', 191);
+            $table->json('achievements');
+            $table->string('avatar', 191);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
