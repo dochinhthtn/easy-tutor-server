7a4475d7aaf7d97a6a6dbdd24a5f5f59a40af1dd
dochinhthtn <dochinhthtn@gmail.com>
2021-03-24 00:02:47 +0700

    Added

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest\LoginRequest;
+
+class AuthController extends Controller {
+    //
+    public function login(LoginRequest $request) {
+
+    }
+
+    public function register() {
+        
+    }
+}
