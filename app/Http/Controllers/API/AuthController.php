8f89bda080431d56a1e708c05991e8b4684c0ecb
dochinhthtn <dochinhthtn@gmail.com>
2021-03-24 00:46:23 +0700

    complete login method

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -8,10 +8,23 @@ use App\Http\Requests\UserRequest\LoginRequest;
 class AuthController extends Controller {
     //
     public function login(LoginRequest $request) {
+        if ($token = auth()->attempt($request->only(['email', 'password']))) {
+            return $this->respondWithToken($token);
+        }
 
+        return response()->json([
+            'login' => 'Your email or password is not correct'
+        ], 401);
     }
 
     public function register() {
-        
+
+    }
+
+    protected function respondWithToken($token) {
+        return response()->json([
+            'token' => $token,
+            'type' => 'bearer',
+        ]);
     }
 }
