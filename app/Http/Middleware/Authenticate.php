7ac18ff756cdc14eff11044713f2ff2c4827a8f3
dochinhthtn <dochinhthtn@gmail.com>
2021-03-25 00:23:47 +0700

    Override unauthenticated method

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,25 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
 
-class Authenticate extends Middleware
-{
+class Authenticate extends Middleware {
+    protected function unauthenticated($request, array $guards) {
+        throw new HttpResponseException(new Response([
+            'message' => 'Unauthorized'
+        ], 401));
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
      */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
+    protected function redirectTo($request) {
+        if (!$request->expectsJson()) {
+            return null;
         }
     }
 }
