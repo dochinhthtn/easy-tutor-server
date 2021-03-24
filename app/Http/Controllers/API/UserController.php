4fb902759533afc3d13b7c8527b437dc5b0bf73b
dochinhthtn <dochinhthtn@gmail.com>
2021-03-25 00:23:24 +0700

    Add updateSubject, updateProfile, getProfile

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest\UpdateProfileRequest;
 
-class UserController extends Controller
-{
+class UserController extends Controller {
     //
+
+    public function updateSubject() {
+
+    }
+
+    public function updateProfile(UpdateProfileRequest $request) {
+        return "update";
+    }
+
+    public function getProfile(User $user) {
+
+    }
 }
