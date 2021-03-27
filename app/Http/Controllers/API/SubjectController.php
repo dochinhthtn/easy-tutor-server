e8b701b63ff4d6d39205b951bc298373d48be2ca
dochinhthtn <dochinhthtn@gmail.com>
2021-03-27 15:55:52 +0700

    Add CRUD methods

--- app/Http/Controllers/API/SubjectController.php
+++ app/Http/Controllers/API/SubjectController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Resources\SubjectResource;
+use App\Models\Subject;
 
-class SubjectController extends Controller
-{
+class SubjectController extends Controller {
     //
+
+    public function getSubjects() {
+        return SubjectResource::collection(Subject::paginate(15));
+    }
+
+    public function getSubject(Subject $subject) {
+        return new SubjectResource($subject);
+    }
+
+    public function addSubject() {
+
+    }
+
+    public function editSubject() {
+
+    }
+
+    public function findSubjects(string $keyword) {
+
+    }
 }
