ab39fa6009031dcac72613dc1853f5c6bf2be13d
dochinhthtn <dochinhthtn@gmail.com>
2021-03-30 11:15:25 +0700

    Modify addPost, editPost method

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest\PostEditorRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use App\Models\User;
@@ -23,24 +24,45 @@ class PostController extends Controller {
 
     public function getRecommendedPosts() {
         $subjectIds = $this->currentUser->subjects()->get(['id'])->toArray();
-        $query = Post::whereIn('subject_id', $subjectIds)->orderBy('created_at', 'desc');
+        $query = Post::whereIn('subject_id', $subjectIds)->orderBy('created_at', 'desc')->with('user');
         return PostResource::collection($query->paginate(15));
     }
 
     public function getAllPosts() {
-        return PostResource::collection(Post::orderBy('created_at', 'desc')->paginate(15));
+        return PostResource::collection(Post::orderBy('created_at', 'desc')->with('user')->paginate(15));
     }
 
     public function getPost(Post $post) {
         return new PostResource($post->with('applicants'));
     }
 
-    public function addPost() {
+    public function addPost(PostEditorRequest $request) {
+        $post = Post::create([
+            'subject_id' => $request->input('subjectId'),
+            'user_id' => $this->currentUser->id,
+            'description' => $request->input('description'),
+            'address' => $request->input('address'),
+            'offer' => $request->input('offer'),
+        ]);
 
+        return new PostResource($post);
     }
 
-    public function editPost() {
+    public function editPost(PostEditorRequest $request, Post $post) {
+        if($post->user_id != $this->currentUser->id) {
+            return response()->json([
+                'message' => 'You are not owned this post'
+            ], 401);
+        }
 
+        $post->update([
+            'subject_id' => $request->input('subjectId'),
+            'description' => $request->input('description'),
+            'address' => $request->input('address'),
+            'offer' => $request->input('offer'),
+        ]);
+
+        return new PostResource($post);
     }
 
     public function applyPost() {
