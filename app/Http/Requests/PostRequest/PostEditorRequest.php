77b4e9dbdc18fef1f3914d298fa09697755ad2e2
dochinhthtn <dochinhthtn@gmail.com>
2021-03-30 11:15:49 +0700

    Added

---
+++ app/Http/Requests/PostRequest/PostEditorRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\PostRequest;
+
+use App\Http\Requests\APIRequest;
+
+class PostEditorRequest extends APIRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'subjectId' => 'required|exists:subjects,id',
+            'description' => 'required',
+            'address' => 'required',
+            'offer' => 'required|numeric'
+        ];
+    }
+}
