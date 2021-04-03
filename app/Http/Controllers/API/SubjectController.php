425c2a202af8bdd582b10f024ee9619ac6584592
dochinhthtn <dochinhthtn@gmail.com>
2021-04-04 01:33:02 +0700

    Completed add, edit, find methods

--- app/Http/Controllers/API/SubjectController.php
+++ app/Http/Controllers/API/SubjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SubjectRequest\SubjectEditorRequest;
 use App\Http\Resources\SubjectResource;
 use App\Models\Subject;
 
@@ -17,15 +18,26 @@ class SubjectController extends Controller {
         return new SubjectResource($subject);
     }
 
-    public function addSubject() {
+    public function addSubject(SubjectEditorRequest $request) {
+        Subject::create([
+            'name' => $request->input('name')
+        ]);
 
+        return response()->json([
+            'message' => 'Create subject successfully'
+        ]);
     }
 
-    public function editSubject() {
-
+    public function editSubject(SubjectEditorRequest $request, Subject $subject) {
+        $subject->name = $request->input('name');
+        $subject->save();
+        return response()->json([
+            'message' => 'Update subject successfully'
+        ]);
     }
 
     public function findSubjects(string $keyword) {
-
+        $query = Subject::whereRaw("name LIKE '%$keyword%'")->paginate(15);
+        return SubjectResource::collection($query);
     }
 }
