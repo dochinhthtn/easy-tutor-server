2d3cee2b10ee061a09cff659cd7fcbfee0398f14
dochinhthtn <dochinhthtn@gmail.com>
2021-04-12 23:58:26 +0700

    Added

---
+++ database/migrations/2021_04_12_164146_create_conversations_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConversationsUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations_users', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('conversation_id')->constrained('conversations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations_users');
+    }
+}
